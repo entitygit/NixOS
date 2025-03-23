55f7e3d8b9f94036c4cf1581b6171de4d0d24e73
installer <installer@gmail.com>
2025-03-23 00:22:39 -0500

    hello

---
+++ home.nix
@@ -0,0 +1,60 @@
+{ config, pkgs, ... }:
+
+{
+  home.username = "linus";
+  home.homeDirectory = "/home/linus";
+
+  home.packages = with pkgs; [
+    fastfetch
+    vesktop
+    vscode
+    neovim
+    fd
+    nil
+    rustup
+    stremio
+    gnome-tweaks
+  ];
+  programs.fish = {
+    enable = true;
+    interactiveShellInit = ''
+      set fish_greeting
+    '';
+  };
+
+  programs.git = {
+    enable = true;
+    userName = "entitygit";
+    userEmail = "linus.luke07@gmail.com";
+  };
+
+  home.file.".config/monitors.xml".text = ''
+    <monitors version="2">
+      <configuration>
+        <layoutmode>physical</layoutmode>
+        <logicalmonitor>
+          <x>0</x>
+          <y>0</y>
+          <scale>1</scale>
+          <primary>yes</primary>
+          <monitor>
+            <monitorspec>
+              <connector>DP-1</connector>
+              <vendor>GBT</vendor>
+              <product>G34WQC</product>
+              <serial>20532B000142</serial>
+            </monitorspec>
+            <mode>
+              <width>3440</width>
+              <height>1440</height>
+              <rate>144.000</rate>
+            </mode>
+          </monitor>
+        </logicalmonitor>
+      </configuration>
+    </monitors>
+    '';
+
+  home.stateVersion = "24.11";
+  programs.home-manager.enable = true;
+}
