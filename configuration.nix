a744b67179d7d4d6a371e991a08c8cb56524ce63
entitygit <98179520+entitygit@users.noreply.github.com>
2025-03-23 04:32:30 -0500

    Cinnamon

--- configuration.nix
+++ configuration.nix
@@ -65,8 +65,8 @@
   services.xserver.enable = true;
 
   # Enable the GNOME Desktop Environment.
-  services.xserver.displayManager.gdm.enable = true;
-  services.xserver.desktopManager.gnome.enable = true;
+  services.xserver.displayManager.lightdm.enable = true;
+  services.xserver.desktopManager.cinnamon.enable = true;
 
   systemd.tmpfiles.rules = [
     "L /run/gdm/.config/monitors.xml - - - - ${./monitors.xml}"
