80fd9727ed0a7fcdc7bdcae10d382633a7e9fe91
entitygit <98179520+entitygit@users.noreply.github.com>
2025-03-23 17:45:02 -0500

    home cursor

--- home.nix
+++ home.nix
@@ -27,6 +27,13 @@
     userEmail = "98179520+entitygit@users.noreply.github.com";
   };
 
+  home.pointerCursor = {
+    gtk.enable = true;
+    package = pkgs.bibata-cursors;
+    name = "Bibata-Modern-Classic";
+    size = 24;
+  };
+
   home.file.".config/monitors.xml".text = ''
     <monitors version="2">
       <configuration>
