8ad5d602c8201f1844b87a9e82f0b31a93cd7627
entitygit <98179520+entitygit@users.noreply.github.com>
2025-03-23 05:48:05 -0500

    hope

--- home.nix
+++ home.nix
@@ -32,12 +32,6 @@
     enable = true;
     userName = "entitygit";
     userEmail = "98179520+entitygit@users.noreply.github.com";
-    extraConfig = {
-      credential = {
-        credentialStore = "secretservice";
-        helper = pkgs.git-credential-manager;
-      };
-    };
   };
 
   home.file.".config/monitors.xml".text = ''
